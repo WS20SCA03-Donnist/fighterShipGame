0c52600fcad9754b0538c3e57b1d0327308e9e8e
Donavin Wats <donnist@gmail.com>
2020-05-04 10:08:09 -0400

    Initial Commit

---
+++ fighterShipGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fighterShipGame
+//
+//  Created by Donavin Watson on 5/4/20.
+//  Copyright © 2020 Don Watsy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
